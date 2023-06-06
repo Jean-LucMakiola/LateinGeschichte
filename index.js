779b83c554c25261a7286870af3769cc48f5a2ce
JeanLucMakiola <makiolajeanluc@gmail.com>
2023-06-06 16:11:28 +0200

    rewamp

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const foundingSection = document.querySelector(".gruendung-section");
 founding.addEventListener("click", () => {
   foundingSection.style.display = "grid";
   foundingSection.scrollIntoView({ behavior: "smooth" });
+  document.getElementById("main").style.display = "none";
 });
 
 const closeButton = document
   .querySelector(".close")
   .addEventListener("click", () => {
     console.log("clicked");
+    document.getElementById("main").style.display = "grid";
+    await(2);
     document.getElementById("main").scrollIntoView({ behavior: "smooth" });
     document.querySelector(".gruendung-section").style.display = "none";
   });
