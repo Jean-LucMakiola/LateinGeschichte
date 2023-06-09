59b744ef30ca863e9e83738478a41f35ba1b54b9
JeanLucMakiola <makiolajeanluc@gmail.com>
2023-06-09 18:02:50 +0200

    dawlhdb

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const cardOne = document.querySelector("._founding-card1");
-cardOne.addEventListener("click", () => {
-  cardOne.classList.add("active");
-});
+const cardOneOpener = document.getElementById("card-button1");
+const cardOneCloseMenu = document.querySelector(".card1_nav");
 
 const cardTwo = document.querySelector("._founding-card2");
-cardTwo.addEventListener("click", () => {
+const cardTwoOpener = document.getElementById("card-button2");
+const cardTwoCloseMenu = document.querySelector(".card2_nav");
+
+cardOneOpener.addEventListener("click", () => {
+  cardOne.classList.add("active");
+});
+cardOneCloseMenu.addEventListener("click", () => {
+  cardOne.classList.remove("active");
+});
+
+
+cardTwoOpener.addEventListener("click", () => {
   cardTwo.classList.add("active");
+});
+cardTwoCloseMenu.addEventListener("click", () => {
+  cardTwo.classList.remove("active");
 });
